1c7f098cce019451f33f250a558667fa4aaf9cc4
SaipraveenSPK <saipraveen.annauniv@gmail.com>
2023-12-24 20:28:20 +0530

    new

---
+++ products.js
@@ -0,0 +1,74 @@
+const products = [
+    {
+        "id": 1,
+        "name": "Smartphone Alpha",
+        "desc": "High-end smartphone with advanced features",
+        "price": 999,
+        "image": "https://picsum.photos/200/300?random=1"
+    },
+    {
+        "id": 2,
+        "name": "Bluetooth Headphones",
+        "desc": "Wireless headphones with noise cancellation",
+        "price": 199,
+        "image": "https://picsum.photos/200/300?random=2"
+    },
+    {
+        "id": 3,
+        "name": "Smart Watch Pro",
+        "desc": "Advanced smartwatch with health tracking",
+        "price": 299,
+        "image": "https://picsum.photos/200/300?random=3"
+    },
+    {
+        "id": 4,
+        "name": "E-Book Reader",
+        "desc": "Lightweight e-book reader with paper-like display",
+        "price": 129,
+        "image": "https://picsum.photos/200/300?random=4"
+    },
+    {
+        "id": 5,
+        "name": "Portable Speaker",
+        "desc": "Compact and powerful portable speaker",
+        "price": 89,
+        "image": "https://picsum.photos/200/300?random=5"
+    },
+    {
+        "id": 6,
+        "name": "Wireless Charger",
+        "desc": "Fast and convenient wireless charger",
+        "price": 59,
+        "image": "https://picsum.photos/200/300?random=6"
+    },
+    {
+        "id": 7,
+        "name": "Fitness Tracker",
+        "desc": "Durable fitness tracker for all sports",
+        "price": 79,
+        "image": "https://picsum.photos/200/300?random=7"
+    },
+    {
+        "id": 8,
+        "name": "Tablet Elite",
+        "desc": "Powerful tablet with high-resolution screen",
+        "price": 499,
+        "image": "https://picsum.photos/200/300?random=8"
+    },
+    {
+        "id": 9,
+        "name": "Virtual Reality Headset",
+        "desc": "Immersive VR headset for gaming and media",
+        "price": 349,
+        "image": "https://picsum.photos/200/300?random=9"
+    },
+    {
+        "id": 10,
+        "name": "Gaming Console",
+        "desc": "Next-gen gaming console with 4K support",
+        "price": 499,
+        "image": "https://picsum.photos/200/300?random=10"
+    }
+];
+
+module.exports = products;
